f60acdbc509a7abba93b04fabe7d1927420eb7b8
joshanaba <anabajoshchi@gmail.com>
2022-06-29 03:56:11 +0100

    added stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,17 @@
 <script>
+import { constants } from "buffer";
 import { ref } from "vue";
 
 export default {
   name: "HomeView",
   setup() {
-    const p = ref(null);
-    let name = "Josh";
+    const name = ref('Josh');
     const handleClick = () => {
-      console.log(p, p.value);
-      p.value.classList.add("class");
-      p.value.innerContent = 'Hello guys'
+      name.value = 'Yo'
     };
     return {
       name,
-      handleClick,
-      p,
+      handleClick
     };
   },
 };
@@ -22,7 +19,8 @@ export default {
 
 <template>
   <main>
-    <p ref="p">my name is {{ name }}</p>
+    <p>my name is {{ name }}
+    </p>
     <button @click="handleClick()">Click me</button>
   </main>
 </template>
