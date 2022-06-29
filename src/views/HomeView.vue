a6fa30f8bd058490ed8ef6351907b458e019867f
joshanaba <anabajoshchi@gmail.com>
2022-06-29 03:45:39 +0100

    added stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
-<script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "HomeView",
+  setup() {
+    const p = ref(null);
+    let name = "Josh";
+    const handleClick = () => {
+      console.log(p, p.value);
+      p.value.classList.add("class");
+      p.value.innerContent = 'Hello guys'
+    };
+    return {
+      name,
+      handleClick,
+      p,
+    };
+  },
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p ref="p">my name is {{ name }}</p>
+    <button @click="handleClick()">Click me</button>
   </main>
 </template>
