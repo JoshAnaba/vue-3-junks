1ad46a7e05802067afe44c8be45daae23b43839c
joshanaba <anabajoshchi@gmail.com>
2022-06-29 10:12:27 +0100

    added stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,45 @@
 <script>
-import { constants } from "buffer";
-import { ref } from "vue";
+import { ref, reactive, computed } from "vue";
 
 export default {
   name: "HomeView",
   setup() {
-    const name = ref('Josh');
+    // data
+    const name = ref("Josh");
+    const age = ref(30);
+    // methods
+    const dataObj = ref({
+      name: "Anaba",
+      age: 40,
+    });
+    const dataObjTwo = reactive({
+      name: "Solo",
+      age: 50,
+    });
+    // computed
+    const computedName = computed(() => {
+      return dataObj.value.name + " " + dataObjTwo.name;
+    });
     const handleClick = () => {
-      name.value = 'Yo'
+      name.value = "Yo";
+    };
+    const updateDataObj = () => {
+      // ref
+      dataObj.value.age++;
+      // reactive, their drawbacks is that primitive values cannot be updated reactively say, const house = reactive('bungalow'), a function cannot update the variable house
+      dataObjTwo.age++;
     };
     return {
+      // data
       name,
-      handleClick
+      age,
+      dataObj,
+      dataObjTwo,
+      // methods
+      handleClick,
+      updateDataObj,
+      // computed
+      computedName
     };
   },
 };
@@ -19,8 +47,19 @@ export default {
 
 <template>
   <main>
-    <p>my name is {{ name }}
-    </p>
-    <button @click="handleClick()">Click me</button>
+    <div>
+      <h1>Ref</h1>
+      <p>my name is {{ dataObj.name }} and I am {{ dataObj.age }} years old</p>
+      <button @click="handleClick()">Click me</button>
+      <input type="text" v-model="dataObj.name" />
+      <button @click="updateDataObj()">Update data obj</button>
+    </div>
+    <div>
+      <h1>Reactive</h1>
+      <p>
+        my name is {{ dataObjTwo.name }} and I am {{ dataObjTwo.age }} years old
+      </p>
+      <p>{{ computedName }}</p>
+    </div>
   </main>
 </template>
